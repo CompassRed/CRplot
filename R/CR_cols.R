0f6b30bd09770de3aa85e14ca074765835dc8d2b
rharrington31 <rmharrington31@gmail.com>
2020-06-11 17:40:19 -0400

    Added details about color parameters and example

--- R/CR_cols.R
+++ R/CR_cols.R
@@ -1,21 +1,20 @@
 #' Function to extract CompassRed colors as hex codes
 #'
 #' @param ... Names of CompassRed colors as strings. Color options include:
-#' \itemize{
-#'  \item{"CR_red"}{#E83536}}
-#'  \item{"CR_blue"}{#171B2C}
-#'  \item{"CR_green"}{#00B050}
-#'  \item{"cb_gray"}{#999999}
-#'  \item{"cb_orange"}{#E69F00}
-#'  \item{"cb_light_blue"}{#56B4E9}
-#'  \item{"cb_green"}{#009E73}
-#'  \item{"cb_yellow"}{#F0E442}
-#'  \item{"cb_dark_blue"}{#0072B2}
-#'  \item{"cb_dark_orange"}{#D55E00}
-#'  \item{"cb_pink"}{#CC79A7}
-#'  \item{"light_gray"}{#CCCCCC}
-#'  \item{"dark_gray"}{#8c8c8c}
-#'  }
+#' * "CR_red" - #E83536
+#' * "CR_blue" - #171B2C
+#' * "CR_green" - #00B050
+#' * "cb_gray" - #999999
+#' * "cb_orange" - #E69F00
+#' * "cb_light_blue" - #56B4E9
+#' * "cb_green" - #009E73
+#' * "cb_yellow" - #F0E442
+#' * "cb_dark_blue" - #0072B2
+#' * "cb_dark_orange" - #D55E00
+#' * "cb_pink" - #CC79A7
+#' * "light_gray" - #CCCCCC
+#' * "dark_gray" - #8c8c8c
+#' @md
 #'
 #' @return Either one or many hex codes as strings.
 #'
