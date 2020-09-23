eb11f3933a6b0251885ad7d51658601068a3f078
rharrington31 <rmharrington31@gmail.com>
2020-09-23 11:20:52 -0400

    added new colors

--- R/colors.R
+++ R/colors.R
@@ -152,3 +152,117 @@ dark_gray <- function() {
   "#8C8C8C"
 }
 
+
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for forest_blues
+#'
+#' @export
+#'
+
+forest_blues <- function() {
+  "#0A3D62"
+}
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for reef_encounter
+#'
+#' @export
+#'
+
+reef_encounter <- function() {
+  "#079992"
+}
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for aurora_green
+#'
+#' @export
+#'
+
+aurora_green <- function() {
+  "#78E08F"
+}
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for dupain
+#'
+#' @export
+#'
+
+dupain <- function() {
+  "#60A3BC"
+}
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for squash_blossom
+#'
+#' @export
+#'
+
+squash_blossom <- function() {
+  "#F6B93B"
+}
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for carrot_orange
+#'
+#' @export
+#'
+
+carrot_orange <- function() {
+  "#E58E26"
+}
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for mandarin_red
+#'
+#' @export
+#'
+
+mandarin_red <- function() {
+  "#E55039"
+}
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for american_river
+#'
+#' @export
+#'
+
+american_river <- function() {
+  "#636E72"
+}
+
+
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for dracula_orchid
+#'
+#' @export
+#'
+
+dracula_orchid <- function() {
+  "#2D3436"
+}
+
+
+#' Function to generate text string of individual colors
+#'
+#' @return This function returns the hex code for city_lights
+#'
+#' @export
+#'
+
+city_lights <- function() {
+  "#DFE6E9"
+}
